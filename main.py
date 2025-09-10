1b32fa0035dc7018efb7d7ccbbe6827e656639f4
Nick393 <82907413+Nick393@users.noreply.github.com>
2025-09-10 16:38:03 -0500

    added adding program

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
     print("Hello from hello-ci-cd!")
+    num1=input("enter a number: ")
+    num2=input("Enter another number: ")
+    sum=int(num1)+int(num2)
+    print(sum)
 
 
 if __name__ == "__main__":
