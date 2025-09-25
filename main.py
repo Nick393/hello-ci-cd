dde58369d7f9e51e2afba4a39c0db3e53f902b36
Nick393 <82907413+Nick393@users.noreply.github.com>
2025-09-24 22:58:58 -0500

    added a workflow with a runner

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ def main():
     print("Hello from hello-ci-cd!")
     num1=input("enter a number: ")
     num2=input("Enter another number: ")
-    sum=int(num1)+int(num2)
+    add(num1,num2)
+    
     print(sum)
-
+def add(num1,num2):
+    sum=int(num1)+int(num2)
+    return sum
 
 if __name__ == "__main__":
     main()
